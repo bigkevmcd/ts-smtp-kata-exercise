c610efd0533d72f7344f3ea915befc27f7083288
Kevin McDermott <bigkevmcd@gmail.com>
2022-08-18 18:37:52 +0100

    Commit code for exercise.

---
+++ README.md
@@ -0,0 +1,3 @@
+# smtp-kata-ts
+
+This is an implementation of my vacation email kata, written in TypeScript with Jest.
---
+++ src/mail.test.ts
@@ -0,0 +1,9 @@
+import { Mock, Times, It } from "moq.ts";
+
+test('user is on vacation', () => {
+  const testMail = {
+    to: ["test1@example.com"],
+    from: "test2@example.com",
+    body: "To: test1@example.com\nSubject: testing\n\nThis is a test"
+  }
+});
